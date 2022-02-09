c328de29c5000d5d861ca6ff6f340bca7aadb445
John Mercouris <john@mercouris.email>
2022-02-09 13:32:24 -0600

    pox.nix: successfully package by using pypi

--- pox.nix
+++ pox.nix
@@ -1,22 +1,18 @@
-{ lib, buildPythonPackage, fetchFromGitHub, pytest }:
+{ lib, buildPythonPackage, fetchurl }:
 
 buildPythonPackage rec {
   pname = "pox";
-  version = "0.0.1";
+  version = "0.3.0";
 
-  src = fetchFromGitHub {
-    owner = "noxrepo";
-    repo = pname;
-    rev = "5f82461e01f8822bd7336603b361bff4ffbd2380";
-    sha256 = "1xdkf99wz9sz8fwc80hl8r0zq4kbn36zs6fsb0s6998pg4ibkb0l";
+  src = fetchurl {
+    url = "https://files.pythonhosted.org/packages/b9/bc/d348b6372b72fea32be294481e068659d277db3fb49be08f7955c5a55425/pox-0.3.0.zip";
+    sha256 = "0cdy58z543gf2va5bvx0gbyscfpchx2hh8asj2s6nil6n58875nb";
   };
-
-  checkInputs = [ pytest ];
 
   meta = with lib; {
     description = "POX is a networking software platform written in Python.";
     homepage = "https://github.com/noxrepo/pox";
-    license = licenses.mit;
+    license = licenses.asl20;
     maintainers = with maintainers; [ jmercouris ];
   };
 
