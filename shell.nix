4ec30508ce92045037432023ed5f1d30ecf162fe
John Mercouris <john@mercouris.email>
2022-02-10 22:15:33 -0600

    shell.nix: load cadcad

--- shell.nix
+++ shell.nix
@@ -1,20 +1,27 @@
 with import <nixpkgs> {};
 
-# define function my-python-package
 let
-  pathos = ps: ps.callPackage ./pathos.nix {
-    pox = ps.callPackage ./pox.nix {};
+  cadcad = ps: ps.callPackage ./cadcad.nix {
     dill = ps.callPackage ./dill.nix {};
+    pox = ps.callPackage ./pox.nix {};
     multiprocess = ps.callPackage ./multiprocess.nix {
       dill = ps.callPackage ./dill.nix {};
     };
     ppft = ps.callPackage ./ppft.nix {};
+
+    pathos = ps.callPackage ./pathos.nix {
+      pox = ps.callPackage ./pox.nix {};
+      dill = ps.callPackage ./dill.nix {};
+      multiprocess = ps.callPackage ./multiprocess.nix {
+        dill = ps.callPackage ./dill.nix {};
+      };
+      ppft = ps.callPackage ./ppft.nix {};
+    };
   };
 
 in
 (pkgs.python38.withPackages (ps: with ps; [
-  # call function my-python-package with argument ps
-  (pathos ps)
+  (cadcad ps)
   ipykernel 
   jupyterlab
 ])).env
