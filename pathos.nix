08ba422a72bd9e79049b222188e50b515d37b341
John Mercouris <john@mercouris.email>
2022-02-10 19:30:14 -0600

    pathos.nix: package pathos for nix

--- pathos.nix
+++ pathos.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchFromGitHub, pytest }:
+{ lib, buildPythonPackage, fetchFromGitHub, pytest, pox, dill, multiprocess, ppft }:
 
 buildPythonPackage rec {
   pname = "pathos";
@@ -12,6 +12,13 @@ buildPythonPackage rec {
   };
 
   checkInputs = [ pytest ];
+
+  buildInputs = [
+    pox
+    dill
+    multiprocess
+    ppft
+  ];
 
   meta = with lib; {
     description = "pathos is a framework for heterogeneous computing. It provides a consistent high-level interface for configuring and launching parallel computations across heterogeneous resources.";
--- shell.nix
+++ shell.nix
@@ -2,15 +2,19 @@ with import <nixpkgs> {};
 
 # define function my-python-package
 let
-  my-python-package = ps: ps.callPackage ./readme.nix {};
+  pathos = ps: ps.callPackage ./pathos.nix {
+    pox = ps.callPackage ./pox.nix {};
+    dill = ps.callPackage ./dill.nix {};
+    multiprocess = ps.callPackage ./multiprocess.nix {
+      dill = ps.callPackage ./dill.nix {};
+    };
+    ppft = ps.callPackage ./ppft.nix {};
+  };
 
 in
 (pkgs.python38.withPackages (ps: with ps; [
   # call function my-python-package with argument ps
-  (my-python-package ps)
+  (pathos ps)
   ipykernel 
   jupyterlab
-  matplotlib
-  numpy
-  pandas
 ])).env
